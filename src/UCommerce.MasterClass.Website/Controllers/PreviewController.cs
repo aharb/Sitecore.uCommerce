051c2862d6aca77e8ddc9259307ca6b2462f92df
Ahmad Harb <ahmed.a.harb@gmail.com>
2016-01-20 18:53:25 +0200

    Preview Order Logic

--- src/UCommerce.MasterClass.Website/Controllers/PreviewController.cs
+++ src/UCommerce.MasterClass.Website/Controllers/PreviewController.cs
@@ -8,25 +8,48 @@ using UCommerce.MasterClass.Website.Models;
 
 namespace UCommerce.MasterClass.Website.Controllers
 {
-	public class PreviewController : System.Web.Mvc.Controller
-	{
-		public ActionResult Index()
-		{
-			PurchaseOrderViewModel model = MapOrder();
-			return View("/Views/preview.cshtml", model);
-		}
+    public class PreviewController : System.Web.Mvc.Controller
+    {
+        public ActionResult Index()
+        {
+            PurchaseOrderViewModel model = MapOrder();
+            return View("/Views/preview.cshtml", model);
+        }
 
-		private PurchaseOrderViewModel MapOrder()
-		{
-			var basketModel = new PurchaseOrderViewModel();
+        private PurchaseOrderViewModel MapOrder()
+        {
+            var basket = TransactionLibrary.GetBasket(false).PurchaseOrder;
 
-			return basketModel;
-		}
-		
-		[HttpPost]
-		public ActionResult Post()
-		{
-			return View("/Views/Complete.cshtml");
-		}
-	}
+            var basketModel = new PurchaseOrderViewModel();
+
+            basketModel.OrderTotal = new Money(basket.OrderTotal.GetValueOrDefault(), basket.BillingCurrency).ToString();
+            basketModel.SubTotal = new Money(basket.SubTotal.GetValueOrDefault(), basket.BillingCurrency).ToString();
+            basketModel.ShippingTotal = new Money(basket.ShippingTotal.GetValueOrDefault(), basket.BillingCurrency).ToString();
+            basketModel.PaymentTotal = new Money(basket.PaymentTotal.GetValueOrDefault(), basket.BillingCurrency).ToString();
+            basketModel.DiscountTotal = new Money(basket.DiscountTotal.GetValueOrDefault(), basket.BillingCurrency).ToString();
+            basketModel.TaxTotal = new Money(basket.TaxTotal.GetValueOrDefault(), basket.BillingCurrency).ToString();
+
+            foreach (var orderLine in basket.OrderLines)
+            {
+                basketModel.OrderLines.Add(new OrderlineViewModel()
+                {
+                    OrderLineId = orderLine.OrderLineId,
+                    ProductName = orderLine.ProductName,
+                    Sku = orderLine.Sku,
+                    VariantSku = orderLine.VariantSku,
+                    Quantity = orderLine.Quantity,
+                    Total = new Money(orderLine.Total.GetValueOrDefault(), basket.BillingCurrency).ToString()
+                });
+            }
+            return basketModel;
+        }
+
+        [HttpPost]
+        public ActionResult Post()
+        {
+            UCommerce.Api.TransactionLibrary.RequestPayments();
+
+            return View("/Views/Complete.cshtml");
+        }
+    }
 }
