8962db6d8ed04f85ef474d4b5fc4646a0ae93e7d
Ahmad Harb <ahmed.a.harb@gmail.com>
2016-01-20 17:51:01 +0200

    Adding Shipping logic

--- src/UCommerce.MasterClass.Website/Controllers/ShippingController.cs
+++ src/UCommerce.MasterClass.Website/Controllers/ShippingController.cs
@@ -8,19 +8,37 @@ using UCommerce.MasterClass.Website.Models;
 
 namespace UCommerce.MasterClass.Website.Controllers
 {
-	public class ShippingController : System.Web.Mvc.Controller
-	{
-		public ActionResult Index()
-		{
-			var shipping = new ShippingViewModel();
+    public class ShippingController : System.Web.Mvc.Controller
+    {
+        public ActionResult Index()
+        {
+            var shippingModel = new ShippingViewModel();
 
-			return View("/Views/Shipping.cshtml", shipping);
-		}
 
-		[HttpPost]
-		public ActionResult Index(ShippingViewModel shipping)
-		{
-			return Redirect("/store/payment");
-		}
-	}
+            shippingModel.AvailableShippingMethods = new List<SelectListItem>();
+            var shippingCountry = TransactionLibrary.GetShippingInformation().Country;
+            var avalableShippingMethods = TransactionLibrary.GetShippingMethods(shippingCountry);
+
+            var selectedShipping = TransactionLibrary.GetBasket(false).PurchaseOrder.Shipments.FirstOrDefault();
+
+            foreach (var avalableShippingMethod in avalableShippingMethods)
+            {
+                shippingModel.AvailableShippingMethods.Add(new SelectListItem()
+                {
+                    Selected = selectedShipping != null && selectedShipping.ShippingMethod.ShippingMethodId == avalableShippingMethod.ShippingMethodId,
+                    Text = avalableShippingMethod.Name,
+                    Value = avalableShippingMethod.ShippingMethodId.ToString()
+                });
+            }
+
+
+            return View("/Views/Shipping.cshtml", shippingModel);
+        }
+
+        [HttpPost]
+        public ActionResult Index(ShippingViewModel shipping)
+        {
+            return Redirect("/store/payment");
+        }
+    }
 }
