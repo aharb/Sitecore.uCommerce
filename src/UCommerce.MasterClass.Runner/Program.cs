30377be26d1ed17ca87d7e80496a5e189b990b32
Ahmad Harb <ahmed.a.harb@gmail.com>
2016-01-21 16:13:18 +0200

    Worked on the Integration project: 1- Using the IOC CONTAINER. 2- Using the static API. 3- Using custom query.

--- src/UCommerce.MasterClass.Runner/Program.cs
+++ src/UCommerce.MasterClass.Runner/Program.cs
@@ -6,6 +6,8 @@ using log4net.Repository.Hierarchy;
 using UCommerce.EntitiesV2;
 using UCommerce.Infrastructure;
 using UCommerce.MasterClass.BusinessLogic.Queries;
+using System.Collections.Generic;
+using NHibernate;
 
 namespace UCommerce.MasterClass.Integration
 {
@@ -15,12 +17,33 @@ namespace UCommerce.MasterClass.Integration
 		{
 			log4net.Config.BasicConfigurator.Configure();
 			
-			var order = ObjectFactory
+            // using IOC container 
+			UCommerce.EntitiesV2.PurchaseOrder order = ObjectFactory
 				.Instance
 				.Resolve<IRepository<PurchaseOrder>>()
-				.Select(new LatestOrderQuery()).ToList();
+				.Select(new LatestOrderQuery()).FirstOrDefault();
 
-			
+            // using static API
+            var oreders = UCommerce.EntitiesV2.PurchaseOrder.All().ToList();
+
+
+            IRepository<UCommerce.EntitiesV2.PurchaseOrder> orederRepository =
+                ObjectFactory.Instance.Resolve<IRepository<UCommerce.EntitiesV2.PurchaseOrder>>();
+
+            // Grbs order using the repository
+            List<PurchaseOrder> orders = orederRepository.Select().ToList();
+
+            ISessionProvider sessionProvider = ObjectFactory.Instance.Resolve<ISessionProvider>();
+
+            ISession session = sessionProvider.GetSession();
+
+
+
+            //using custom query 
+            var products = ObjectFactory.Instance.Resolve<IRepository<Product>>().Select(new ProductByPropertiesQuery("ShowOnHomePage", "True"));
+
+
+
 		}
 	}
 }
